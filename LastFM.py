f5bf3283d29a9653268568cbbb5ad59f24f5ffb3
Justin DeGuzman <justindeguzman@dhcp-172-31-049-201.mobile.uci.edu>
2026-03-09 10:26:23 -0700

    Created necessary structure for LastFM class.

--- LastFM.py
+++ LastFM.py
@@ -3,3 +3,19 @@
 # 72329664
 
 # lastfm.py
+
+class LastFM:
+    def __init__(self):
+        pass
+
+    def _download_url(self, url_to_download: str) -> dict:
+        pass
+
+    def set_apikey(self, apikey: str) -> None:
+        pass
+
+    def load_data(self) -> None:
+        pass
+
+    def transclude(self, message: str) -> str:
+        pass
