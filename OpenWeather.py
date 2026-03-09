47bae8d263727b555fbd6421d2b77c53006e2391
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-03-08 20:36:33 -0700

    Created class skeleton structure.

--- OpenWeather.py
+++ OpenWeather.py
@@ -3,3 +3,24 @@
 # 72329664
 
 # openweather.py
+
+class OpenWeather:
+    def __init__(self, zipcode: str, ccode: str) -> None:
+        self.zipcode = zipcode
+        self.ccode = ccode
+
+        self.temperature = None
+        self.high_temperature = None
+        self.low_temperature = None
+        self.longitude = None
+        self.latitude = None
+        self.description = None
+        self.humidity = None
+        self.city = None
+        self.sunset = None
+
+    def set_apikey():
+        pass
+
+    def load_data():
+        pass
