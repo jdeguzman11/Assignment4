b71a5aaaec076407a76be7dbf582b864ec623ab6
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-03-08 20:44:44 -0700

    Moved test function for download url to be used in load_data function.

--- OpenWeather.py
+++ OpenWeather.py
@@ -4,6 +4,11 @@
 
 # openweather.py
 
+import urllib
+import json
+from urllib import request, error
+
+
 class OpenWeather:
     def __init__(self, zipcode: str, ccode: str) -> None:
         self.zipcode = zipcode
@@ -25,3 +30,22 @@ class OpenWeather:
 
     def load_data(self) -> None:
         pass
+
+    def _download_url(self, url_to_download: str) -> dict:
+    response = None
+    r_obj = None
+
+    try:
+        response = urllib.request.urlopen(url_to_download)
+        json_results = response.read()
+        r_obj = json.loads(json_results)
+
+    except urllib.error.HTTPError as e:
+        print('Failed to download contents of URL')
+        print('Status code: {}'.format(e.code))
+
+    finally:
+        if response is not None:
+            response.close()
+
+    return r_obj
