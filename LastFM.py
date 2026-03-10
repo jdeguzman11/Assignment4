01f775b24229bebd12a9a357e2f4aabc4f0b4e80
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-03-09 17:04:17 -0700

    Refactored LastFM.py similarly to OpenWeather but having it inherit from WebAPI.

--- LastFM.py
+++ LastFM.py
@@ -9,35 +9,13 @@
 import urllib
 import json
 from urllib import request, error
+from WebAPI import WebAPI
 
 
-class LastFM:
+class LastFM(WebAPI):
     def __init__(self):
         self.api_key = None
         self.artist = None
-
-    def _download_url(self, url_to_download: str) -> dict:
-        response = None
-        r_obj = None
-
-        try:
-            response = urllib.request.urlopen(url_to_download)
-            json_results = response.read()
-            r_obj = json.loads(json_results)
-
-        except urllib.error.HTTPError as e:
-            print('Failed to download contents of URL')
-            print('Status code: {}'.format(e.code))
-
-        finally:
-            if response is not None:
-                response.close()
-
-        return r_obj
-
-    def set_apikey(self, apikey: str) -> None:
-        self.api_key = apikey
-        pass
 
     def load_data(self) -> None:
         try:
